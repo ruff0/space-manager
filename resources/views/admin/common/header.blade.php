71cb28d59b20634bed1b7a5131c20a9632120b78
Boudy de Geer <boudydegeer@mosaiqo.com>
2016-04-11 22:47:03 +0200

    Clean up a little bit

--- resources/views/admin/common/header.blade.php
+++ resources/views/admin/common/header.blade.php
@@ -7,17 +7,6 @@
 				<span class="text-semibold">{{ $current['model'] }}</span> - {{ $current['action'] }}
 			</h4>
 		</div>
-
-		{{--<div class="heading-elements">--}}
-			{{--<div class="heading-btn-group">--}}
-				{{--<a href="#" class="btn btn-link btn-float text-size-small has-text"><i--}}
-						{{--class="icon-bars-alt text-primary"></i><span>Statistics</span></a>--}}
-				{{--<a href="#" class="btn btn-link btn-float text-size-small has-text"><i class="icon-calculator text-primary"></i>--}}
-					{{--<span>Invoices</span></a>--}}
-				{{--<a href="#" class="btn btn-link btn-float text-size-small has-text"><i class="icon-calendar5 text-primary"></i>--}}
-					{{--<span>Schedule</span></a>--}}
-			{{--</div>--}}
-		{{--</div>--}}
 		<div class="heading-elements">
 			<a href="@yield('new-form-url', '#')" class="btn bg-blue btn-labeled heading-btn legitRipple">
 				<b>
