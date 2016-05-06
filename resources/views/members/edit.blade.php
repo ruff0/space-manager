ace6e2332a85c25b22abb799bf7fc391f9d41dfd
Boudy de Geer <boudydegeer@mosaiqo.com>
2016-05-06 20:51:32 +0200

    Add current plan widget

--- resources/views/members/edit.blade.php
+++ resources/views/members/edit.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('body-class', '')
+
 @section('page-scripts')
 	{{--<script src="/js/page/home.js"></script>--}}
 @endsection
@@ -51,6 +53,64 @@
 							</div>
 						</div>
 						{{--/UserProfile--}}
+
+						{{-- Current Plan --}}
+						{{--<div class="panel panel-flat">--}}
+						<div class="panel panel-flat border-left-lg border-left-danger invoice-grid timeline-content">
+							<div class="panel-body">
+								<div class="row">
+									<div class="col-sm-6">
+										<h6 class="text-semibold no-margin-top">{{$member->companyName()}}</h6>
+										<ul class="list list-unstyled">
+											<li>Plan actual: <span class="text-semibold">{{$member->currentPlan()->name}}</span></li>
+										</ul>
+									</div>
+
+									<div class="col-sm-6">
+										<h6 class="text-semibold text-right no-margin-top">{{$member->companyIdentity()}}</h6>
+										<ul class="list list-unstyled text-right">
+											<li class="dropdown">
+												<div class="label
+														{!! $member->active ? 'label-success' : 'label-danger' !!}">
+														{!! $member->active ? 'Activo' : 'Inactivo' !!}
+												</div>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="panel-footer panel-footer-condensed">
+								<div class="heading-elements">
+									<span class="heading-text">
+										<span class="status-mark border-danger position-left"></span>
+										Miembro desde:
+										<span class="text-semibold">
+											{{$member->created_at->format('d/m/Y')}}
+										</span>
+									</span>
+
+									<ul class="list-inline list-inline-condensed heading-text pull-right">
+										<li class="dropdown">
+											<a href="#" class="text-default dropdown-toggle" data-toggle="dropdown">
+												<i class="icon-menu7"></i>
+												<span class="caret"></span>
+											</a>
+											<ul class="dropdown-menu dropdown-menu-right">
+												<li><a href="#"><i class="icon-file-eye"></i> Ver Factura</a></li>
+												<li><a href="#"><i class="icon-file-download"></i> Descargar factura</a></li>
+												<li class="divider"></li>
+												<li><a href="#"><i class="icon-cross2"></i> Cancelar suscripción</a></li>
+											</ul>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						{{--</div>--}}
+						{{-- /Current Plan --}}
+
+
 						{{-- Navigation --}}
 						<div class="panel panel-flat">
 							<div class="list-group no-border">
